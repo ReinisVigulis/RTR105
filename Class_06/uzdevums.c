1f9c0480b22c3d42e5ba53b058881c11d1a78557
ReinisVigulis <sinier2004@gmail.com>
2023-10-04 09:57:03 +0300

    Class_

---
+++ Class_06/uzdevums.c
@@ -0,0 +1,21 @@
+
+}#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+
+{
+
+    int CurrentYear;
+    int BornYear;
+    int age = CurrentYear - BornYear;
+
+    printf("What year is it?\n");
+    scanf("%d", &CurrentYear);
+    //printf("Year check: %d\n", CurrentYear);
+    printf("You are %d years old\n", age);
+    scanf("%d", $Years old);
+    //printf("Year check: %d\n", BornYear);
+    printf("You are %d years old\n", age);
+    return 0;
+}
