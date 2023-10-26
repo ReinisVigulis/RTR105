5e81ccb6307c812aee8e7e0ee0f15fdbe97ae893
ReinisVigulis <sinier2004@gmail.com>
2023-10-26 19:37:14 +0300

    Class_uzdevums1a

---
+++ majasdarbi/majasdarbs1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main() {
+   int long long skaitlis1, skaitlis2, rezultats;
+
+    printf("Ierakstiet pirmo skaitli 400 000 (četri simti tūkstoši) : ");
+    scanf("%lld", &skaitlis1);
+
+    printf("Ieraksiet otro skaitli 100 000 (simts tūkstoši) : ");
+    scanf("%lld", &skaitlis2);
+
+    rezultats = skaitlis1 * skaitlis2;
+    if(rezultats == 40000000000) {
+    
+    printf("Reizinājums ir: %lld\n", rezultats);
+}
+    else { 
+    printf("Ievad'itas nepareizas v'ert'ibas, jo reizin'ajums san'ak: %lld\n", rezultats);
+}
+    return 0;
+}
