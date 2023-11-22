b9f85284893cf7ed70dd355b9db0f62e128636e6
ReinisVigulis <sinier2004@gmail.com>
2023-11-22 12:05:41 +0200

    18nodarbiba

---
+++ labdarb2.2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+float modified_sin2(float x, float A){
+    return sin(x * x)-A;}
+
+    void main(){
+        float a, x, delta_x, b, y, A;
+        a = 0;
+        b = 2*M_PI;
+
+        printf("Cien. liet., luudzu, ievadiet A veertibu sekojošajam vienadojumam:sin(x * x)=A\n");
+        scanf("%f", &A);
+        x = a;
+        delta_x = 0.1;
+        printf("\tx\ty\n");
+        while(x<b)
+        {
+            printf("%10.1f%10.1f\n", x, modified_sin2(x,A));
+            x += delta_x;
+        }
+
+
+    }
---
+++ labdarb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    float a = 0.01, b = 1.5 * M_PI, x, delta_x = 1.e-3/*0.0001*/, funkca, funkcb, funkcx;
+    int k = 0;
+    funkca = sin (a * a); funkcb = sin (b * b);
+    if (funkca * funkcb >0){
+        printf("intervaala [%.2f;%.2f] sin(x * x) funkcijai ",a ,b);
+        printf("saknju nav (vai taaja ir paaru saknju skaits)\n");
+        return 1;}
+
+    printf("               sin(%7.3f)=%7.3f\t\t\t\t", a, sin (a * a));
+    printf("sin(%7.3f)=%7.3f\n", b, sin (b * b));
+
+    while((b-a) > delta_x){
+        k++;
+        x = (a+b) / 2.;
+        if(funkca * sin (x * x) > 0)
+        a = x;
+    else
+    b = x;
+    printf("%2d. interacija: sin (%7.3f) = %7.3f\t", k, a, sin(a * a));
+    printf("sin(%7.3f) = %7.3f\t", x, sin(x * x));
+    printf("sin(%7.3f) = %7.3f\n", b, sin(b * b));}
+
+    printf("Saakne atrodas pie x = %.3f, jo sin(x) ir %.3f\n", x, sin (x * x));
+    return 0;}
