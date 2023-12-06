19142aaef02de75981afc51c7589f6444b934bc3
ReinisVigulis <sinier2004@gmail.com>
2023-12-06 11:53:57 +0200

    skola

--- labdarb2.3.c
+++ labdarb2.3.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-    float a = 1.4, b = 2.0, x, A, delta_x = 1.e-3/*0.0001*/, funkca, funkcb, funkcx;
+    float a, b, x, A, delta_x, funkca, funkcb, funkcx;
     int k = 0;
     printf("Cien. liet., luudzu, ievadiet A veertibu sekojošajam vienadojumam:sin(x * x)=A\n");
     scanf("%f", &A);
 
+    printf("Cien. liet., lūdzu ievadiet vērtību a: \n");
+    scanf("%f", &a);
+
+    printf("Ievadiet vērtību b: \n");
+    scanf("%f", &b);
+
+
+    printf("Ievadiet delta x vērtību: \n");
+    scanf("%f", &delta_x);
     funkca = sin (a * a)-A; funkcb = sin (b * b)-A;
     if (funkca * funkcb >0){
         printf("intervaala [%.2f;%.2f] sin(x * x) funkcijai ",a ,b);
@@ -21,14 +30,14 @@ int main(){
         if(funkca * (sin (x * x)-A) > 0)
         a = x;
     else
-    b = x;A;
+        b = x;
     printf("%2d. interacija: sin (%7.3f) = %7.3f\t", k, a, sin(a * a)-A);
     printf("sin(%7.3f) = %7.3f\t", x, sin(x * x)-A);
     printf("sin(%7.3f) = %7.3f\n", b, sin(b * b)-A);
     
     }
 
-    printf("Saakne atrodas pie x = %.3f, jo sin(x) ir %.3f\n", x, sin (x * x)-A);
+    printf("Saakne atrodas pie x = %.3f, jo sin(x) ir %.3f\n", x, sin (x * x));
     return 0;
     
 }
